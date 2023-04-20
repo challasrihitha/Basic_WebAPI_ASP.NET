78475440d403d43c91e1989e7025ba2e49feb3ed
srihithareddy <challasrihitha98@gmail.com>
2023-04-20 15:47:13 -0500

    added usermodel in Models and loginController in Controller

---
+++ MVCWebApp/Models/UserModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MVCWebApp.Models
+{
+    public class UserModel
+    {
+        public int id { get; set; }
+        public string? username { get; set; }
+        public string? password { get; set; }
+    }
+}
+
